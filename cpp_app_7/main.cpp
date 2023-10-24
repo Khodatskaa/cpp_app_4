826b2526f0af1739b44157afa0bf0861dabd0cce
Khodatskaa <khodatskaalina@gmail.com>
2023-10-24 13:41:54 +0300

    Ex 1 was done

--- cpp_app_7/main.cpp
+++ cpp_app_7/main.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
+	cout << "E ti porto con me" << endl;
+	cout << "Dove il cielo si fa buio" << endl;
+	cout << "Dove BLANCO è poi cresciuto" << endl;
+	cout << "Dove tutto è maledetto, eh, eh" << endl;
+	cout << "E ti porto con me" << endl;
+	cout << "Perché ho paura da solo" << endl;
+	cout << "Perché violenti il mio sfogo" << endl;
+	cout << "Però ne ho troppo bisogno, oh" << endl;
+	cout << endl;
+	cout << "Artist: BLANCO" << endl;
 
 	return 0;
 }
