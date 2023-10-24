2b20ff076748922c88fdcde28fbc9c19c9c9b89e
Khodatskaa <khodatskaalina@gmail.com>
2023-10-24 14:33:17 +0300

    Ex 3 was done

--- cpp_app_7/main.cpp
+++ cpp_app_7/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
-int main()
+int main() 
 {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	return 0;
+    SetConsoleTextAttribute(hConsole, 12);
+    cout << "**************************" << endl;
+    cout << "*         SALE           *" << endl;
+    cout << "**************************" << endl;
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY);
+    cout << "Product: Red-eared freshwater turtle" << endl;
+    cout << "Price: $55.99" << endl;
+    cout << "Description: Selling 5 red-eared freshwater turtles. Delivery available around the town." << endl;
+    cout << "Contact us: by email support@gmail.com" << endl;
+    cout << "\t    or phone +380965587399" << endl;
+
+    return 0;
 }
